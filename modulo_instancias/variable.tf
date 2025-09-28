450abf96cf888835994daed2565aa6817429d14f
luc4s023 <lucas.s023@outlook.com>
2025-09-28 13:14:15 -0300

    feat: modulos

---
+++ modulo_instancias/variable.tf
@@ -0,0 +1,11 @@
+variable "nome" {
+  description = "Nome da instancia"
+  type        = string
+  default     = "projetoa"
+}
+
+variable "env" {
+  description = "Ambiente"
+  type        = string
+  default     = "lab"
+}
